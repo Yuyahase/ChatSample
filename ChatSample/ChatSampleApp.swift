6f4f1f3497894181c576b92ace7e02cc871242ad
haseyuy <haseyuy@gmail.com>
2022-01-26 18:58:53 +0900

    Initial Commit

---
+++ ChatSample/ChatSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSampleApp.swift
+//  ChatSample
+//
+//  Created by 長谷侑弥 on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
